5761b4bd0cdfc088f45497ac1e3d22e625eadbca
Aditya Sinha <sinha.aditya02@gmail.com>
2021-06-02 20:28:43 -0400

    Initial Commit

---
+++ CPP_Bootcamp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPP_Bootcamp
+//
+//  Created by Aditya Sinha on 02/06/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
